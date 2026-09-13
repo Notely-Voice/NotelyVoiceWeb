57c0639753bcd77e1b20c6f54c574b5409cd9e94
Tosin <tosinmath007@gmail.com>
2026-09-13 08:53:05 +0100

    Comment out the HomeDownload

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
   return (
     <div className="overflow-x-hidden">
       <HomeHero  />
-      <HomeDownload />
+      {/* <HomeDownload /> */}
       {/* <HomeTyping /> */}
       <HomeTranscription />
       {/* <HomeWork /> */}
